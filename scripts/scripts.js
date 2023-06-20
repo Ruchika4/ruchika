cd508dc84ffd574ca68692a4f3bd18c00162d76b
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2023-06-20 14:11:32 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import { setLibs } from './utils.js';
 const STYLES = '';
 
 // Use '/libs' if your live site maps '/libs' to milo's origin.
-const LIBS = '/libs';
+const LIBS = 'https://milo.adobe.com/libs';
 
 // Add any config options.
 const CONFIG = {
